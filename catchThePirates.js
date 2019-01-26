081e57037d45195ee76dbbe510452d235a1fbfb0
MohnnadBahaa <mohbah026@gmail.com>
2019-01-26 15:17:39 +0200

    <fix> style and commit code

--- catchThePirates.js
+++ catchThePirates.js
@@ -3,15 +3,22 @@ module.exports.catchThePirates = pirateFaces => {
   var nose = ["-", "~"]; // not have
   var mouth = [")", "|"]; // must
   var count = 0;
+  // loop inside pirateFaces
   for (let i = 0; i < pirateFaces.length; i++) {
+    // split currFace to arr to divide all face parts
     var currFace = pirateFaces[i].split("");
+    // store size to check if the face have nose or not
     var size = currFace.length;
+    // check size if does not have nose
     if (size === 2) {
+      // check if valid face
       if (eyes.includes(currFace[0]) && mouth.includes(currFace[1])) {
         count++;
       }
+      // check size if have nose
     } else if (size === 3) {
       if (
+        // check if valid face
         eyes.includes(currFace[0]) &&
         nose.includes(currFace[1]) &&
         mouth.includes(currFace[2])
@@ -20,5 +27,6 @@ module.exports.catchThePirates = pirateFaces => {
       }
     }
   }
+  // valid face count
   return count;
 };
